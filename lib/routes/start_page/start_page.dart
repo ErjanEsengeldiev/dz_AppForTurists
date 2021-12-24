6987861385f70b6820154354140557ecbe0baf39
ErjanEsengeldiev <erjanesengeldiev@gmail.com>
2021-12-24 16:15:52 +0600

    Add SplashScreen

--- lib/routes/start_page/start_page.dart
+++ lib/routes/start_page/start_page.dart
@@ -1,21 +1,30 @@
+import 'dart:async';
+
+import 'package:dz_ambulance/routes/slider_gid/slider_gid.dart';
 import 'package:flutter/material.dart';
 
-class StartPage extends StatelessWidget {
+class StartPage extends StatefulWidget {
   const StartPage({Key? key}) : super(key: key);
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 2),
+        () => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (_) => const SliderGid())));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          const Center(
-            child: Image(image: AssetImage('assets/images/image.png')),
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.pushReplacementNamed(context, '/sliderGid');
-            },
-          )
-        ],
+    return const Scaffold(
+      body: Center(
+        child: Image(image: AssetImage('assets/images/image.png')),
       ),
     );
   }
