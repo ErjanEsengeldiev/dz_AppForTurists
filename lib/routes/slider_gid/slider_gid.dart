c00ab025dba27b12b41abee1d0eb2006bb01fdd3
ErjanEsengeldiev <erjanesengeldiev@gmail.com>
2021-12-27 14:13:58 +0600

    clear slider_git 1

--- lib/routes/slider_gid/slider_gid.dart
+++ lib/routes/slider_gid/slider_gid.dart
@@ -1,52 +1,6 @@
 import 'package:dz_ambulance/colors/color.dart';
 import 'package:flutter/material.dart';
 
-class _PageForSlider {
-  int id;
-  String title;
-  String imageRoud;
-  Color color;
-  InkWell button;
-  _PageForSlider({
-    required this.button,
-    required this.id,
-    required this.imageRoud,
-    required this.title,
-    required this.color,
-  });
-}
-
-List<_PageForSlider> _pagesList = [
-  _PageForSlider(
-      button: const InkWell(),
-      id: 0,
-      imageRoud: 'assets/images/Group.png',
-      color: Colors.black,
-      title:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis aliquam id id in pharetra lorem. Purus tortor velit sed consequat urna, ipsum. Sit nullam ultrices sed morbi sed. Et blandit commodo vel blandit praesent sed donec ultrices a. Vel, laoreet in et quam elit, sed viverra faucibus.Proin nisi amet urna purus vivamus felis cursus nulla.'),
-  _PageForSlider(
-      button: const InkWell(),
-      id: 1,
-      imageRoud: 'assets/images/Group.png',
-      title: '2',
-      color: Colors.black),
-  _PageForSlider(
-      button: const InkWell(),
-      id: 2,
-      imageRoud: 'assets/images/Group.png',
-      title: '3',
-      color: Colors.black),
-  _PageForSlider(
-      button: InkWell(
-        child: const Text('Next Page'),
-        onTap: () {},
-      ),
-      id: 3,
-      imageRoud: 'assets/images/Group.png',
-      title: 'Нажмите что бы перейти на следующий экран',
-      color: Colors.black),
-];
-
 final PageController pageController = PageController();
 int indexOfPage = 0;
 
@@ -80,33 +34,93 @@ class _SliderGidState extends State<SliderGid> {
         SizedBox(
           height: MediaQuery.of(context).size.height * 0.6,
           child: PageView.builder(
-            controller: pageController,
-            scrollDirection: Axis.horizontal,
-            itemCount: _pagesList.length,
-            itemBuilder: (_, currentIndex) => Container(
-              padding: const EdgeInsets.symmetric(horizontal: 60),
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 60),
-                    child: Image(
-                        image: AssetImage(_pagesList[currentIndex].imageRoud)),
-                  ),
-                  ForNavigator(
-                    id: currentIndex,
-                    child: Text(_pagesList[currentIndex].title),
-                  )
-                ],
-              ),
-            ),
-          ),
+              controller: pageController,
+              scrollDirection: Axis.horizontal,
+              itemCount: _pagesList.length,
+              itemBuilder: (_, currentIndex) => _PageForm(
+                    imageRoud: _pagesList[currentIndex].imageRoud,
+                    text: _pagesList[currentIndex].title,
+                    isTrue: currentIndex == 3 ? true : false,
+                  )),
         ),
         _buildBullets(),
       ]),
     );
   }
 }
+
+class _PageForm extends StatelessWidget {
+  final bool isTrue;
+  final String imageRoud;
+  final String text;
+  const _PageForm({
+    this.isTrue = false,
+    required this.imageRoud,
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 60),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 60),
+            child: Image(image: AssetImage(imageRoud)),
+          ),
+          Text(text),
+          isTrue == true
+              ? ElevatedButton(
+                  child: Text('Push'),
+                  onPressed: () {},
+                )
+              : const SizedBox(),
+        ],
+      ),
+    );
+  }
+}
+
+class _PageForSlider {
+  int id;
+  String title;
+  String imageRoud;
+  Color color;
+
+  _PageForSlider({
+    required this.id,
+    required this.imageRoud,
+    required this.title,
+    required this.color,
+  });
+}
+
+List<_PageForSlider> _pagesList = [
+  _PageForSlider(
+      id: 0,
+      imageRoud: 'assets/images/Group.png',
+      color: Colors.black,
+      title:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis aliquam id id in pharetra lorem. Purus tortor velit sed consequat urna, ipsum. Sit nullam ultrices sed morbi sed. Et blandit commodo vel blandit praesent sed donec ultrices a. Vel, laoreet in et quam elit, sed viverra faucibus.Proin nisi amet urna purus vivamus felis cursus nulla.'),
+  _PageForSlider(
+      id: 1,
+      imageRoud: 'assets/images/Group.png',
+      title: '2',
+      color: Colors.black),
+  _PageForSlider(
+      id: 2,
+      imageRoud: 'assets/images/Group.png',
+      title: '3',
+      color: Colors.black),
+  _PageForSlider(
+      id: 3,
+      imageRoud: 'assets/images/Group.png',
+      title: 'Нажмите что бы перейти на следующий экран',
+      color: Colors.black),
+];
 
 class ForNavigator extends StatelessWidget {
   final int id;
